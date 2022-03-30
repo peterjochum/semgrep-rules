b86be5dc9d8fe41d6630f2525673c04e014233a8
Leland Liu <14846866+lejliu@users.noreply.github.com>
2022-03-29 17:30:24 -0700

    Added express-tainted-sendfile

---
+++ javascript/express/security/audit/express-tainted-sendfile.ts
@@ -0,0 +1,45 @@
+module.exports = function servePublicFiles () {
+  return ({ params, query }: Request, res: Response, next: NextFunction) => {
+    const file = params.file
+
+    if (!file.includes('/')) {
+      verify(file, res, next)
+    } else {
+      res.status(403)
+      next(new Error('File names cannot contain forward slashes!'))
+    }
+  }
+
+  function verify (file, res, next) {
+    if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
+      // ruleid: express-tainted-sendfile
+      res.sendFile(path.resolve('ftp/', file))
+    } else {
+      res.status(403)
+      next(new Error('Only .md and .pdf files are allowed!'))
+    }
+  }
+}
+
+module.exports = function servePublicFiles () {
+  return ({ params, query }: Request, res: Response, next: NextFunction) => {
+    const file = params.file
+
+    if (!file.includes('/')) {
+      verify(file, res, next)
+    } else {
+      res.status(403)
+      next(new Error('File names cannot contain forward slashes!'))
+    }
+  }
+
+  function verify (file, res, next) {
+    if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
+      // ok
+      res.sendFile(path.resolve('ftp/'))
+    } else {
+      res.status(403)
+      next(new Error('Only .md and .pdf files are allowed!'))
+    }
+  }
+}
