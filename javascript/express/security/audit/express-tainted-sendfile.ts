6e7485c8220c0d4ed5945090ad843a1441445156
Leland Liu <--global>
2022-04-01 15:44:26 -0700

    Updated tainted sendfile example

--- javascript/express/security/audit/express-tainted-sendfile.ts
+++ javascript/express/security/audit/express-tainted-sendfile.ts
@@ -21,19 +21,19 @@ module.exports = function servePublicFiles () {
   }
 }
 
-module.exports = function servePublicFiles () {
+module.exports = function servePublicFiles2 () {
   return ({ params, query }: Request, res: Response, next: NextFunction) => {
     const file = params.file
 
     if (!file.includes('/')) {
-      verify(file, res, next)
+      verify2(file, res, next)
     } else {
       res.status(403)
       next(new Error('File names cannot contain forward slashes!'))
     }
   }
 
-  function verify (file, res, next) {
+  function verify2 (file, res, next) {
     if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
       // ok
       res.sendFile(path.resolve('ftp/'))
